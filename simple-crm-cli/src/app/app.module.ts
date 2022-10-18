60b9fcb7e3efacc4b4c7e94bf087c4ab5c586702
colburncodes <colburnsanders@gmail.com>
2022-10-18 15:25:36 -0500

    customer service created

--- simple-crm-cli/src/app/app.module.ts
+++ simple-crm-cli/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { CustomerModule } from './customer/customer.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
@@ -17,6 +18,7 @@ import { CustomerModule } from './customer/customer.module';
     AppRoutingModule,
     CustomerModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     MatToolbarModule,
     MatIconModule,
     MatListModule,
---
+++ simple-crm-cli/src/app/customer/customer.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Customer } from './customer.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CustomerService {
+  constructor(private http: HttpClient) {}
+
+  search(term: string): Observable<Customer[]> {
+    return this.http.get<Customer[]>('/api/customer/search?term=' + term);
+  }
+
+  insert(customer: Customer): Observable<Customer> {
+    return this.http.post<Customer>('/api/customer/save', customer);
+  }
+
+  update(customer: Customer): Observable<Customer> {
+    return this.http.put<Customer>(
+      `/api/customer/${customer.customerId}`,
+      customer
+    );
+  }
+}
